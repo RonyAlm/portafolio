d5a2718a6a97fd3e0cb1813aa6f9bbff32038c48
Ronaldo Almirón <rony.almiron.015@gmail.com>
2025-01-03 08:20:20 -0300

    Reorganizar la estructura de carpetas; actualizar rutas de imágenes en index.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
+(function () {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+      publicKey: "TJN6G39z8-PebUZTc",
+    });
+  })();
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    const form = document.getElementById('form');
+    const btnSubmit = document.getElementById('sendBtn');
+    const formError = document.querySelector('.form__error');
+
+    form.addEventListener('submit', (event) => {
+
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        if (!name || !email || !message || email.indexOf('@') === -1) {
+            messageError('Por favor, completá todos los campos.');
+            return;
+        }
+
+        btnSubmit.textContent = 'Enviando...';
+
+        emailjs.sendForm('service_t4ajffw', 'template_ohghvs8', form)
+            .then((res) => {
+                if (res.status === 200) {
+                    btnSubmit.innerHTML = 'Mensaje enviado correctamente <img src="./assets/icon/check-circle.svg" alt="Send icon">';
+                    form.reset();
+                    setTimeout(() => {
+                        btnSubmit.innerHTML = 'Enviar <img src="./assets/icon/send.svg" alt="Send icon">';
+                    }, 3000);
+                }
+            }, (error) => {
+                messageError('Ocurrió un error al enviar el mensaje');
+                btnSubmit.innerHTML = 'Enviar <img src="./assets/icon/send.svg" alt="Send icon">';
+                form.reset();
+            });
+
+    });
+
+    function messageError(texto) {
+        formError.style.display = 'flex';
+        formError.querySelector('p').textContent = texto;
+        setTimeout(() => {
+            formError.style.display = 'none';
+        }, 4000);
+    }
+
+});
